ede8742bd9bf762c72ead0d1a3ea047cd1e01acd
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-04-05 18:18:22 -0700

    completed outcome with refernce to solution notes

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ function changeColors() {
   }
 
 
+function validateNumber(evt) {
+    el.preventDefault();
+
+    const numberInput = document.querySelector('#number-input');
+    const userNum = Number(numberInput.value);
+
+    const formFeedback = document.querySelector('#formFeedback');
+    if (isNaN(userNum) || userNum >= 10) {
+        formFeedback.innerText = 'Please enter a smaller number';
+    } else {
+        formFeedback.innerText = 'You are good to go'
+    }
+}
+
+document.querySelector('.color-changer').addEventListener('click', changeColor);
+document.querySelector('.number-form').addEventListener('submit', validateNumber);
+
