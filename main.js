1bec66fb682667a6de0f06495d70207a665ae34b
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-04-05 17:58:50 -0700

    change colors

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 'use strict';
+function changeColors() {
+    const colorChangeEls = document.querySelectorAll('.color-change');
+
+    for (const el of colorChangeEls) {
+      el.classList.add('red');
+    }
+  }
+
+
